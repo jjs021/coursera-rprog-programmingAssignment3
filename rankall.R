60f835b81973b9b873124535da957201c4934c0e
jjs021 <mmarkets@nycbmmmbjsohn.home>
2015-05-02 17:45:16 -0400

    Finished rankall submits correctly

--- rankall.R
+++ rankall.R
@@ -11,4 +11,22 @@ rankall <- function(outcome, num = "best",
   ## For each state, find the hospital of the given rank
   ## Return a data frame with the hospital names and the
   ## (abbreviated) state name
+  col <- outcomes[[outcome]]
+  outcomeData <- outcomeData[order(
+    outcomeData[, 7], outcomeData[, col], outcomeData[, 2]),]
+  stateOutcomes <- split(outcomeData, outcomeData[, 7])
+  hospital <- lapply(stateOutcomes, function (d) {
+    d <- subset(d, !is.na(d[, col]))
+    if (num == "best") {
+      d[1, 2]
+    } else {
+      if (num == "worst") {
+        tail(d[, 2], n = 1)
+      } else {
+        d[num, 2]
+      }
+    }
+  })
+  state <- names(hospital)
+  as.data.frame(cbind(hospital, state))
 }
