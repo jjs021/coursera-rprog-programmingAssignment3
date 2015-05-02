980826b3454b741097b90495d166a86df940aa96
jjs021 <mmarkets@nycbmmmbjsohn.home>
2015-05-02 04:36:50 -0400

    rank hospital needs to fix order call

--- rankhospital.R
+++ rankhospital.R
@@ -1,7 +1,25 @@
 source("common.R")
-rankhospital <- function(state, outcome, num = "best") {
+rankhospital <- function(state, outcome, num = "best",
+                         datadir=NULL, csv="outcome-of-care-measures.csv") {
   ## Read outcome data
+  outcomeData <- readCsvDir(datadir, csv)
+  ## Convert data for the 30 day mortality of the outcomes to numeric
+  envValsAsNumeric(outcomeData, outcomes)
   ## Check that state and outcome are valid
+  stopifbad(state, outcome, outcomeData)
+  if (num != "best" && num != "worst" && is.na(as.numeric(num))) {
+        stop("invalid num")
+      }
   ## Return hospital name in that state with the given rank
   ## 30-day death rate
+  col <- outcomes[[outcome]]
+  outcomeState <- order(subset(outcomeData, outcomeData[, 7] == state), col, 2)
+  if (num == "best") {
+    outcomeState[1, 2]
+  } else {
+    if (num == "worst") {
+      tail(outcomeState[, 2], n = 1)
+    }
+  }
+  outcomeState[num, 2]
 }
